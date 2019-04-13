81669f54a4c58c529f4b7917acfe750c79cdad8f
Christopher Dennis <cdennis.aus@gmail.com>
2019-04-13 02:50:33 -0500

    Initial commit of components without tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import Header from './components/header/Header';
+import './app.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>React and Jest</h1>
+        <Header />
       </div>
     );
   }
---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="header-container">
+                <div className="header-logo">
+                    <h1>Logo</h1>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
